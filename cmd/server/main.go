54cfd39550db037890f08cc48d3fc9ea21a8241f
DarRo9 <dev.it.dar@gmail.com>
2025-05-21 01:16:13 +0300

    wip

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,28 +1,23 @@
 package main
 
 import (
-	"flag"
 	"log"
 
-	"websocket-chat/server"
+	"WebSockets_Chat/server"
 
 	"go.uber.org/zap"
 )
 
 func main() {
-	port := flag.Int("port", 8000, "port to listen on")
-	flag.Parse()
-
 	logger, err := zap.NewProduction()
 	if err != nil {
-		log.Fatalf("can't initialize zap logger: %v", err)
+		log.Fatalf("Failed to create logger: %v", err)
 	}
 	defer logger.Sync()
 
 	s := server.NewServer()
-	logger.Info("Starting chat server", zap.Int("port", *port))
-
-	if err := s.Run(*port, logger); err != nil {
+	logger.Info("Starting chat server", zap.Int("port", 8000))
+	if err := s.Run(8000, logger); err != nil {
 		logger.Fatal("Failed to start server", zap.Error(err))
 	}
 }
